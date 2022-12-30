cb6a33c2f133159a8bc3ceb4af9434cd8742b371
Vitali Prakapenka <myitvit@gmail.com>
2022-12-30 10:40:57 +0300

    Added Composition relationship between Order and OrderItems

--- ACM.BL/Order.cs
+++ ACM.BL/Order.cs
@@ -1,22 +1,30 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace ACM.BL
 {
     public class Order
     {
-        public int OrderId { get; private set; }
-
-        public DateTimeOffset? OrderDate { get; set; }
-
-        public Order()
+        public Order() : this(0)
         {
 
         }
 
         public Order(int orderId)
         {
-            this.OrderId = orderId;
+            OrderId = orderId;
+            OrderItems = new List<OrderItem>();
         }
+
+        public int OrderId { get; private set; }
+
+        public DateTimeOffset? OrderDate { get; set; }
+
+        public int CustomerId { get; set; }
+
+        public int ShippingAddressId { get; set; }
+
+        public List<OrderItem> OrderItems { get; set; }
 
         /// <summary>
         /// Validates the order data.
