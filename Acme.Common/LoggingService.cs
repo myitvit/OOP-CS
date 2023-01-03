ad135eab26fc296e3bc68ed7140990ad7b1e289d
Vitali Prakapenka <myitvit@gmail.com>
2023-01-03 14:15:54 +0300

    Added Logging Service

---
+++ Acme.Common/LoggingService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Acme.Common
+{
+    public static class LoggingService
+    {
+        public static void WriteToFile(List<ILoggable> items)
+        {
+            foreach (var item in items)
+            {
+                Console.WriteLine(item.Log());
+            }
+        }
+    }
+}
